081a056acf3efe93cdf480bb03a3e42f27e49296
elthorne <46898565+elthorne@users.noreply.github.com>
2023-05-10 16:01:42 +0100

    feat: Odd or Even Kata

---
+++ katas/odd_or_even.py
@@ -0,0 +1,27 @@
+"""
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+
+Have fun!
+"""
+
+
+def odd_or_even(arr):
+    if not all(isinstance(number, int) for number in arr):
+        raise TypeError("Input must be a list of integers")
+
+    return 'even' if sum(arr) % 2 == 0 else 'odd'
---
+++ tests/katas/test_odd_or_even.py
@@ -0,0 +1,40 @@
+import pytest
+
+from katas.odd_or_even import odd_or_even
+
+
+@pytest.mark.parametrize("input_array", [
+    [0],
+    [0, -1, -5],
+    [0, 1, 3],
+    [1023, 1, 2],
+])
+def test_odd_or_even_returns_even(input_array):
+    # act
+    result = odd_or_even(input_array)
+
+    # assert
+    assert result == "even"
+
+
+@pytest.mark.parametrize("input_array", [
+    [0, 1, 4],
+    [0, 1, 2],
+    [0, 1, 6],
+])
+def test_odd_or_even_returns_odd(input_array):
+    # act
+    result = odd_or_even(input_array)
+
+    # assert
+    assert result == "odd"
+
+
+@pytest.mark.parametrize("input_array", [
+    "foo, bar and things",
+    1
+])
+def test_get_raises_type_error_when_input_is_not_an_int(input_array):
+    # arrange, act and assert
+    with pytest.raises(TypeError):
+        odd_or_even(input_array)
