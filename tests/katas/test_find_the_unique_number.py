8412816b55ef8a783b2961cfdcdf6af859907037
elthorne <46898565+elthorne@users.noreply.github.com>
2023-05-10 16:01:06 +0100

    refactor: Rename variable

--- tests/katas/test_find_the_unique_number.py
+++ tests/katas/test_find_the_unique_number.py
@@ -3,14 +3,14 @@ import pytest
 from katas.find_the_unique_number import find_uniq
 
 
-@pytest.mark.parametrize("input, expected_output", [
+@pytest.mark.parametrize("input_array, expected_output", [
     ([1, 1, 1, 2, 1, 1, 3, 3, 3], 2),
     ([0, 0, 0.55, 0, 0], 0.55),
     ([3, 10, 3, 3, 3], 10),
 ])
-def test_find_uniq(input, expected_output):
+def test_find_uniq(input_array, expected_output):
     # act
-    result = find_uniq(input)
+    result = find_uniq(input_array)
 
     # assert
     assert result == expected_output
