2668002f0265d9761db8d0940db0d1912e79b207
elthorne <46898565+elthorne@users.noreply.github.com>
2023-05-10 14:45:14 +0100

    fix: Remove old function import

--- tests/katas/test_digit_x_digit.py
+++ tests/katas/test_digit_x_digit.py
@@ -1,6 +1,6 @@
 import pytest
 
-from katas.digit_x_digit import square_digits_one_liner, square_digits_using_a_class, square_digits_verbose_original
+from katas.digit_x_digit import square_digits_one_liner, square_digits_verbose_original
 
 
 def test_square_digits_one_liner_returns_concatenated_squared_result_of_every_digit():
@@ -40,45 +40,6 @@ def test_square_digits_one_liner_only_accepts_an_integer():
     # arrange, act and assert
     with pytest.raises(TypeError):
         square_digits_one_liner("765")
-
-
-def test_square_digits_using_a_class_returns_concatenated_squared_result_of_every_digit():
-    # arrange
-    input = 765
-
-    # act
-    result = square_digits_using_a_class(input)
-
-    # assert
-    assert result == 493625
-
-
-def test_square_digits_using_a_class_returns_0_when_an_input_of_0_is_given():
-    # arrange
-    input = 0
-
-    # act
-    result = square_digits_using_a_class(input)
-
-    # assert
-    assert result == 0
-
-
-def test_square_digits_using_a_class_returns_integer():
-    # arrange
-    input = 765
-
-    # act
-    result = square_digits_using_a_class(input)
-
-    # assert
-    assert type(result) == int
-
-
-def test_square_digits_using_a_class_only_accepts_an_integer():
-    # arrange, act and assert
-    with pytest.raises(TypeError):
-        square_digits_using_a_class("765")
 
 
 def test_square_digits_verbose_returns_concatenated_squared_result_of_every_digit():
