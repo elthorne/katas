573a2bef4aebabe4fcbfc3db047e4a3b14362bd0
elthorne <46898565+elthorne@users.noreply.github.com>
2023-05-12 09:03:02 +0100

    refactor: Revert back to basics...
    ...so I can apply the rule of Object Calisthenics one by one

--- katas/your_order_please.py
+++ katas/your_order_please.py
@@ -13,36 +13,16 @@ Examples
 """
 
 
-class UnorderedSentence:
-    def __init__(self, sentence: str):
-        self.list = sentence.split()
-        self.length = len(self.list) + 1
-
-
-class Words:
-    def __init__(self, unordered_sentence: UnorderedSentence):
-        self.unordered_sentence = unordered_sentence
-        self.ordered_list_of_words = []
-        self.ordered_sentence = ""
-
-        self.__orderize()
-        self.__format_output()
-
-    def __orderize(self):
-        for number in range(self.unordered_sentence.length):
-            self.__generate_ordered_list_of_words(number)
-
-    def __generate_ordered_list_of_words(self, number):
-        for word in self.unordered_sentence.list:
-            self.__append_ordered_word(number, word)
-
-    def __append_ordered_word(self, number, word):
-        if str(number) in word:
-            self.ordered_list_of_words.append(word)
-
-    def __format_output(self):
-        self.ordered_sentence = " ".join(self.ordered_list_of_words)
-
-
 def order(sentence):
-    return Words(UnorderedSentence(sentence)).ordered_sentence
+    if not sentence:
+        return ""
+
+    unordered_list_of_words = sentence.split()
+    ordered_list_of_words = []
+
+    for count in range(len(unordered_list_of_words)+1):
+        for word in unordered_list_of_words:
+            if str(count) in word:
+                ordered_list_of_words.append(word)
+
+    return " ".join(ordered_list_of_words)
