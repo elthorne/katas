352f38343b1017298a9d708f69b398e753a34ec3
elthorne <46898565+elthorne@users.noreply.github.com>
2023-05-16 11:22:13 +0100

    docs: include link to Kata

--- katas/pete_the_baker.py
+++ katas/pete_the_baker.py
@@ -1,4 +1,6 @@
 """
+Link: https://www.codewars.com/kata/525c65e51bf619685c000059
+
 Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths.
 Can you help him to find out, how many cakes he could bake considering his recipes?
 
